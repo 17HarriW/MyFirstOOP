f9220911b035959ae8100091063661a3d8594013
William Harris <17HarriW@fulford.york.sch.uk>
2022-09-21 10:05:04 +0100

    Add project files.

---
+++ MyFirstOOP/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student alice = new Student();
+            Student bob = new Student();
+            alice.name = "Alice";
+            alice.age = 15;
+            bob.name = "Bob";
+            bob.age = 17;
+            bob.toggleAtSchool();
+            alice.showInfo();
+            bob.showInfo();
+            
+            Console.ReadLine();
+        }
+    }
+
+    class Student
+    {
+        public string name;
+        public int age;
+        private bool atSchool = false;
+
+        public void showInfo()
+        {
+            Console.WriteLine(name + ", " + age);
+        }
+        public void toggleAtSchool()
+        {
+            atSchool = !(atSchool);
+        }
+        public bool getAtSchool()
+        {
+            return atSchool;
+        }
+    }
+}
